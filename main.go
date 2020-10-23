0bc0b1259fb9e00f20fca36dc94d2f190ee6a364
Angelo Polotto <angelo.polotto@tcs.com>
2020-10-23 18:15:45 -0300

    close #2 windows watcher bug fixed

--- main.go
+++ main.go
@@ -5,6 +5,12 @@
 
 // https://github.com/fyne-io/fyne/issues/941
 // https://github.com/fyne-io/fyne/blob/86d26ebe4d97a525aa5cf1b6720186fc76d3b669/cmd/fyne_demo/screens/window.go
+
+// PR
+// https://github.com/fyne-io/fyne/pull/1222
+// https://github.com/fyne-io/fyne/pull/1449
+// https://github.com/fyne-io/fyne/issues/21
+// https://github.com/fyne-io/fyne/blob/master/cmd/fyne_demo/screens/widget.go#L100
 package main
 
 import (
@@ -17,6 +23,10 @@ import (
 	"strings"
 )
 
+func uriToPath(uri string) string {
+	return strings.Replace(uri, "file://", "", 1)
+}
+
 func openFileDialog(w fyne.Window, fileFormats []string, chosen func(filePath string)) {
 	fd := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
 		if err == nil && reader == nil {
@@ -26,17 +36,32 @@ func openFileDialog(w fyne.Window, fileFormats []string, chosen func(filePath st
 			dialog.ShowError(err, w)
 			return
 		}
-		filePath := strings.Replace(reader.URI().String(), "file://", "", 1)
+		filePath := uriToPath(reader.URI().String())
 		chosen(filePath)
 	}, w)
 	fd.SetFilter(storage.NewExtensionFileFilter(fileFormats))
 	fd.Show()
 }
 
+func openFolderDialog(w fyne.Window, chosen func(folderPath string)) {
+	dialog.ShowFolderOpen(func(list fyne.ListableURI, err error) {
+		if err != nil {
+			dialog.ShowError(err, w)
+			return
+		}
+		if list == nil {
+			return
+		}
+		folderPath := uriToPath(list.String())
+		chosen(folderPath)
+	}, w)
+}
+
 func main() {
 	var watchStr = map[string]string{}
 	watchStr["disabled"] = "Start watch folder and download subtitle automatically"
 	watchStr["enabled"] = "Stop watch folder and download subtitle automatically"
+	var watchStarted = false
 
 	var appMain = app.New()
 	var w = appMain.NewWindow("Subtitle watcher")
@@ -46,7 +71,7 @@ func main() {
 	openFileBtn := widget.NewButton("Download subtitle for a file", func() {
 		openFileDialog(w, FileFormats, func(filePath string) {
 			downloadSubtitle(filePath, func() {
-				dialog.ShowInformation("Done!", "Subtitle downloaded for the file: " + filePath, w)
+				dialog.ShowInformation("Done!", "Subtitle downloaded for the file: "+filePath, w)
 			}, func(err error) {
 				dialog.ShowError(err, w)
 			})
@@ -55,22 +80,33 @@ func main() {
 
 	var watchFolderBtn *widget.Button
 	watchFolderBtn = widget.NewButton(watchStr["disabled"], func() {
-		progress := dialog.NewProgressInfinite("Folder watcher", "Starting folder watcher, please wait...", w)
-		progress.Show()
-		subtitleWatcher(func(home string, name string) {
-			watchFolderBtn.Text = watchStr["enabled"]
-			watchFolderBtn.Style = widget.PrimaryButton
-			progress.Hide()
-			dialog.ShowInformation("Folder watcher", "Watching files in the dir '"+home+"' of the user: "+name, w)
-		}, func() {
-			watchFolderBtn.Text = watchStr["disabled"]
-			watchFolderBtn.Style = widget.DefaultButton
-			progress.Hide()
-			dialog.ShowInformation("Folder watcher", "Folder watcher stopped", w)
-		}, func(err error) {
-			progress.Hide()
-			dialog.ShowError(err, w)
-		})
+		watchStarted = !watchStarted
+		if watchStarted {
+			openFolderDialog(w, func(folderPath string) {
+				progress := dialog.NewProgressInfinite("Folder watcher", "Starting folder watcher, please wait...", w)
+				progress.Show()
+				subtitleWatcherStart(folderPath, func(folderPath string) {
+					watchFolderBtn.Text = watchStr["enabled"]
+					watchFolderBtn.Importance = widget.HighImportance
+					watchFolderBtn.Refresh()
+					progress.Hide()
+					dialog.ShowInformation("Folder watcher", "Watching files in the dir '"+folderPath, w)
+				}, func(err error) {
+					progress.Hide()
+					dialog.ShowError(err, w)
+				})
+			})
+		} else {
+			progress := dialog.NewProgressInfinite("Folder watcher", "Starting folder watcher, please wait...", w)
+			progress.Show()
+			subtitleWatcherStop(func() {
+				watchFolderBtn.Text = watchStr["disabled"]
+				watchFolderBtn.Importance = widget.MediumImportance
+				watchFolderBtn.Refresh()
+				progress.Hide()
+				dialog.ShowInformation("Folder watcher", "Folder watcher stopped", w)
+			})
+		}
 	})
 
 	//grid := fyne.NewContainerWithLayout(layout.NewGridWrapLayout(fyne.NewSize(250, 250)), openFileBtn, watchFolderBtn)
--- maincontroller.go
+++ maincontroller.go
@@ -4,23 +4,12 @@ import (
 	"fmt"
 	"github.com/radovskyb/watcher"
 	"log"
-	"os/user"
 	"subtitlewatcher/folderwatcher"
 	"subtitlewatcher/subtitledb"
 )
 
-func userInfo() (string, string, error) {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-		return "", "", err
-	}
-	return  usr.HomeDir, usr.Name, nil
-}
-
 var FileFormats = []string{".avi", ".mkv", ".mp4", ".m4v", ".mov", ".mpg", ".wmv"}
 var languages = []string{"pt", "en"}
-var watchStarted = false
 var localWatcher *watcher.Watcher
 
 func downloadSubtitle(filePath string, onSuccess func(), onError func(err error)) {
@@ -32,29 +21,23 @@ func downloadSubtitle(filePath string, onSuccess func(), onError func(err error)
 	}
 }
 
-func subtitleWatcher(enabled func(home string, name string), disabled func(), onError func(err error)) {
-	watchStarted = !watchStarted
-	if watchStarted {
-		home, name, err := userInfo()
+func subtitleWatcherStart(folderPath string, onSuccess func(folderPath string), onError func(err error)) {
+	localWatcher = folderwatcher.New()
+	err := folderwatcher.Watch(localWatcher, FileFormats, folderPath, func(filePath string) {
+		fmt.Printf(filePath)
+		err := subtitledb.Get(languages, filePath)
 		if err != nil {
-			onError(err)
-		} else {
-			localWatcher = folderwatcher.New()
-			err := folderwatcher.Watch(localWatcher, FileFormats, home, func(filePath string) {
-				fmt.Printf(filePath)
-				err := subtitledb.Get(languages, filePath)
-				if err != nil {
-					log.Print(err)
-				}
-			})
-			if err != nil {
-				onError(err)
-			} else {
-				enabled(home, name)
-			}
+			log.Print(err)
 		}
+	})
+	if err != nil {
+		onError(err)
 	} else {
-		folderwatcher.Stop(localWatcher)
-		disabled()
+		onSuccess(folderPath)
 	}
+}
+
+func subtitleWatcherStop(onSuccess func()) {
+	folderwatcher.Stop(localWatcher)
+	onSuccess()
 }
