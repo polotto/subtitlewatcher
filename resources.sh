97b67a1598aae44fd1e819527c322a8d614594a2
Angelo Polotto <angelo.polotto@tcs.com>
2020-11-18 17:53:44 -0300

    give the user option to download subtitle clicking with right button on the video file (Windows)

--- resources.sh
+++ resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p ./resources/tmp/locales
-fyne bundle -package "locales" -prefix "Res" ./resources/locales > ./resources/tmp/locales/locales.go
+"$GOPATH"/bin/fyne bundle -package "locales" -prefix "Res" ./resources/locales > ./resources/tmp/locales/locales.go
 mkdir -p ./resources/tmp/images
-fyne bundle -package "images" -prefix "Res" ./resources/images > ./resources/tmp/images/images.go
+"$GOPATH"/bin/fyne bundle -package "images" -prefix "Res" ./resources/images > ./resources/tmp/images/images.go
