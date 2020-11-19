7623c31d4487c16ecd58ae925fd9dc2c63459c44
Angelo Polotto <angelo.polotto@tcs.com>
2020-11-19 10:58:25 -0300

    Ci Update

--- resources.sh
+++ resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p ./resources/tmp/locales
-"$GOROOT"/bin/fyne bundle -package "locales" -prefix "Res" ./resources/locales > ./resources/tmp/locales/locales.go
+"$(go env GOPATH)"/bin/fyne bundle -package "locales" -prefix "Res" ./resources/locales > ./resources/tmp/locales/locales.go
 mkdir -p ./resources/tmp/images
-"$GOROOT"/bin/fyne bundle -package "images" -prefix "Res" ./resources/images > ./resources/tmp/images/images.go
+"$(go env GOPATH)"/bin/fyne bundle -package "images" -prefix "Res" ./resources/images > ./resources/tmp/images/images.go
