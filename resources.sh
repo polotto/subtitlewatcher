6e1574541d966a719f8475d332cd93ce7d5f13b2
Angelo Polotto <angelo.polotto@tcs.com>
2020-11-19 10:41:57 -0300

    Ci Update

--- resources.sh
+++ resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p ./resources/tmp/locales
-"$GOPATH"/bin/fyne bundle -package "locales" -prefix "Res" ./resources/locales > ./resources/tmp/locales/locales.go
+"$GITHUB_PATH"/bin/fyne bundle -package "locales" -prefix "Res" ./resources/locales > ./resources/tmp/locales/locales.go
 mkdir -p ./resources/tmp/images
-"$GOPATH"/bin/fyne bundle -package "images" -prefix "Res" ./resources/images > ./resources/tmp/images/images.go
+"$GITHUB_PATH"/bin/fyne bundle -package "images" -prefix "Res" ./resources/images > ./resources/tmp/images/images.go
