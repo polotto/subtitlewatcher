c3261cccc936231cdc3b65d7b40939e26d12882f
Angelo Polotto <angelo.polotto@tcs.com>
2020-12-07 15:19:09 -0300

    Updates
    - multiplatform compilation correction
    - release script updted
    - README added logo
    - removed github action

--- releaseCross.sh
+++ releaseCross.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# GO111MODULE=on go get github.com/lucor/fyne-cross/v2/cmd/fyne-cross
-
 NAME_BIN="subtitlewatcher"
 APP_ID="subtitlewatcher/polotto/com.github"
 
 ./resources.sh
 
-fyne-cross windows -arch=amd64 -icon "icon.png"  -output "$NAME_BIN"_windows.exe &&
-fyne-cross linux -arch=amd64  -icon "icon.png" -output "$NAME_BIN"_linux &&
-fyne-cross darwin -arch=amd64  -icon "icon.png" -output "$NAME_BIN"_darwin -app-id "$APP_ID"
+DIST="./fyne-cross/dist"
 
-MAC_DIST="./fyne-cross/dist/darwin-amd64/"
+NAME_WIN="$NAME_BIN""_windows"
+NAME_LIN="$NAME_BIN""_linux"
+NAME_DAR="$NAME_BIN""_darwin"
 
-zip -r "$MAC_DIST""$NAME_BIN"_darwin.zip "$MAC_DIST""$NAME_BIN"_darwin.app
+fyne-cross windows -arch=amd64 -icon "icon.png"  -output "$NAME_WIN".exe &&
+fyne-cross linux -arch=amd64  -icon "icon.png" -output $NAME_LIN &&
+fyne-cross darwin -arch=amd64  -icon "icon.png" -output $NAME_DAR -app-id "$APP_ID"
+
+zip -r "$DIST/$NAME_DAR".zip "$DIST/darwin-amd64/$NAME_DAR".app
+cp "$DIST/linux-amd64/subtitlewatcher_linux.tar.gz" "$DIST/subtitlewatcher_linux.tar.gz"
+cp "$DIST/windows-amd64/subtitlewatcher_windows.exe.zip" "$DIST/subtitlewatcher_windows.zip"
