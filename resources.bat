8dd467654f6517eb68d518efc6e6f7305f122064
Ângelo Polotto <angelopolotto@gmail.com>
2020-12-07 14:32:11 -0300

    windows resources

---
+++ resources.bat
@@ -0,0 +1,6 @@
+@echo off
+
+mkdir -p .\resources\tmp\locales
+fyne bundle -package locales -prefix Res .\resources\locales > .\resources\tmp\locales\locales.go
+mkdir -p .\resources\tmp\images
+fyne bundle -package images -prefix Res .\resources\images > .\resources\tmp\images\images.go
